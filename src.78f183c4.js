e7932dd3a9185c12505122ea7fc3759ff3f03167
Alf-540 <88378124+Alf-540@users.noreply.github.com>
2021-11-14 12:46:10 +0000

    Deploying to gh-pages from @ Alf-540/kiss@7b39ab41398248718e413d3d86343b5e04d70963 🚀

--- src.78f183c4.js
+++ src.78f183c4.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/review.svg":[["review.d42deeb5.svg","xFUB"],"xFUB"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/kiss/src.f3f3aef0.js.map
+//# sourceMappingURL=/kiss/src.78f183c4.js.map
