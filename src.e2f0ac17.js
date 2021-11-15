87215961bc5c4b1d10b5157f730c2d6cc5d149e5
Alf-540 <88378124+Alf-540@users.noreply.github.com>
2021-11-15 05:05:55 +0000

    Deploying to gh-pages from @ Alf-540/kiss@865dd0f98c6f1b78b12a6966469aa186c8172e3a 🚀

--- src.e2f0ac17.js
+++ src.e2f0ac17.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"/home/runner/work/kiss/kiss/src/images/advantages/advantages-bucket.png":[["advantages-bucket.dd7c127f.png","Dewk"],"Dewk"],"/home/runner/work/kiss/kiss/src/images/advantages/advantages-apples.png":[["advantages-apples.41c1b850.png","Mg6x"],"Mg6x"],"/home/runner/work/kiss/kiss/src/images/advantages/advantages-lollipop.png":[["advantages-lollipop.8a71e61b.png","Cb99"],"Cb99"],"./../images/review.svg":[["review.d42deeb5.svg","xFUB"],"xFUB"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/kiss/src.e67f77b7.js.map
+//# sourceMappingURL=/kiss/src.e2f0ac17.js.map
